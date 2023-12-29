259e1d972ef3323a09955579e05882b7a1a0af6a
husainasfak <asfakhusain99@gmail.com>
2023-12-29 07:13:44 +0530

    add shadcn/ui

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ pnpm + React + Ts + vite + import alias
 
 pnpm add -D sax @types/node
 pnpm add sax -D tailwindcss postcss autoprefixer
+npx tailwindcss init -p
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import reactLogo from '@assets/react.svg'
+import { Button } from './components/ui/button'
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <p className="text-3xl text-red-600">
         Click on the Vite and React logos to learn more
+        <Button>Click me</Button>
       </p>
     </>
   )
